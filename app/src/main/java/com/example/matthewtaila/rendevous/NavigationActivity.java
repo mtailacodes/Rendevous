2c13739a87ed66f66b78f498be7efa7b0e582e70
MTailaCodes <mtailacodes@gmail.com>
2017-08-10 22:02:21 -0400

    Added <layout> tag in navigation XML to allow for data binding. Also added navigation button to navigate to CameraActivity

--- app/src/main/java/com/example/matthewtaila/rendevous/NavigationActivity.java
+++ app/src/main/java/com/example/matthewtaila/rendevous/NavigationActivity.java
@@ -1,13 +1,37 @@
 package com.example.matthewtaila.rendevous;
 
+import android.content.Intent;
+import android.database.DatabaseUtils;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class NavigationActivity extends AppCompatActivity {
+import com.example.matthewtaila.rendevous.activity.CameraActivity;
+import com.example.matthewtaila.rendevous.databinding.ActivityNavigationBinding;
+
+public class NavigationActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivityNavigationBinding mActivityNavigationBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
+        mActivityNavigationBinding = DataBindingUtil.setContentView(this, R.layout.activity_navigation);
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        mActivityNavigationBinding.cameraActivityButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cameraActivityButton:
+                Intent intent = new Intent(this, CameraActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
